64dd5a105a6982e6842be7583016e251a811125c
Paul Greidanus <paul@majestik.org>
2021-12-30 23:12:33 -0700

    Basic functionality for brine/prod works

--- ecoforest_vars.go
+++ ecoforest_vars.go
@@ -13,16 +13,16 @@ type dataPoint struct {
 	ini      int
 	reg      int
 	index    int
-	metric   prometheus.Metric
+	metric   prometheus.Gauge
 }
 
 func setCounter(funcType int, function int, ini int, reg int, index int, val float64) {
-	var metric prometheus.Metric
+	var metric prometheus.Gauge
 	for _, dataPoint := range dataPoints {
 		if dataPoint.funcType == funcType && dataPoint.function == function && dataPoint.ini == ini && dataPoint.reg == reg && dataPoint.index == index {
 			metric = dataPoint.metric
 			fmt.Println(reflect.TypeOf(&metric))
-			&metric.Set(val)
+			metric.Set(val)
 		}
 	}
 }
@@ -33,7 +33,7 @@ var dataPoints = []dataPoint{
 		function: 2002,
 		ini:      1,
 		reg:      39,
-		index:    1,
+		index:    2,
 		metric: promauto.NewGauge(prometheus.GaugeOpts{
 			Name: "ecoforest_brine_input_gauge",
 			Help: "Temperature in C of the brine input temperature",
@@ -44,10 +44,32 @@ var dataPoints = []dataPoint{
 		function: 2002,
 		ini:      1,
 		reg:      39,
-		index:    2,
+		index:    3,
 		metric: promauto.NewGauge(prometheus.GaugeOpts{
 			Name: "ecoforest_brine_output_gauge",
 			Help: "Temperature in C of the brine output temperature",
+		}),
+	},
+	dataPoint{
+		funcType: 1,
+		function: 2002,
+		ini:      1,
+		reg:      39,
+		index:    4,
+		metric: promauto.NewGauge(prometheus.GaugeOpts{
+			Name: "ecoforest_production_input_gauge",
+			Help: "Temperature in C of the production input temperature",
+		}),
+	},
+	dataPoint{
+		funcType: 1,
+		function: 2002,
+		ini:      1,
+		reg:      39,
+		index:    5,
+		metric: promauto.NewGauge(prometheus.GaugeOpts{
+			Name: "ecoforest_production_output_gauge",
+			Help: "Temperature in C of the production output temperature",
 		}),
 	},
 }
